e6d2a8be1220985fd78a01cfdc0a73203481dd41
chetran <114576865+chetran@users.noreply.github.com>
2022-11-12 20:45:49 +0100

    Add files via upload

---
+++ LinkedListNames.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+typedef struct linkedlist
+{
+    char *name;
+    struct linkedlist * next;
+}
+linkedlist;
+
+linkedlist* makelist(char* first);
+linkedlist* insertPerson(linkedlist* list, char* newperson);
+void printList(linkedlist* list);
+void delList(linkedlist* list);
+bool find(linkedlist* list, char* person);
+
+int main(void)
+{
+    linkedlist *newlist = makelist("Jhon");
+    newlist = insertPerson(newlist, "bobafetttttt");
+    printList(newlist);
+    printf("Is Jakob is this list (0=NO/1=YES): %i\n", find(newlist, "Jakob"));
+    delList(newlist);
+    return 0;
+}
+
+linkedlist* makelist(char* first)
+{
+    linkedlist* newList = malloc(sizeof(linkedlist));
+    if (newList == NULL)
+    {
+        return NULL;
+    }
+
+    newList->name = first;
+    newList->next = NULL;
+
+    return newList;
+}
+
+linkedlist* insertPerson(linkedlist* list, char* newperson)
+{
+    linkedlist* newP = malloc(sizeof(linkedlist));
+    if (newP == NULL)
+    {
+        return NULL;
+    }
+
+    newP->name = newperson;
+    newP->next = list;
+
+    return newP;
+}
+
+void printList(linkedlist* list)
+{
+    linkedlist* temp = list;
+    while (temp->next != NULL)
+    {
+        printf("%s\n", temp->name);
+        temp = temp->next;
+    }
+    printf("%s\n", temp->name);
+}
+
+bool find(linkedlist* list, char* person)
+{
+    linkedlist* temp = list;
+    while (temp->next != NULL)
+    {
+        if (strcmp(temp->name, person) == 0)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    if (strcmp(temp->name, person) == 0)
+        {
+            return true;
+        }
+    return false;
+}
+
+void delList(linkedlist* list)
+{
+    if (list->next == NULL)
+    {
+        return free(list);
+    }
+
+    delList(list->next);
+
+    return free(list);
+}
+
+
+
+
